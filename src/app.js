3d9996a7f3b3e63920bc7ab72ae7e42fc33aa566
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-01-24 03:05:42 +0530

    Modified _base.scss file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<div>This is boilerplate</div>, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
