0ba3844734994ce8d815022940a1da8c4f11e16e
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-01-24 02:57:53 +0530

    Added simple boilerplate consisting of index.html, basic standard generic styling, webpac and required npm library and modules

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<div>This is boilerplate</div>, document.getElementById('app'));
