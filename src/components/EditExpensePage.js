cf2a7a0b38c102f2d58f9af8c35eef8f062c55c6
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-01-24 03:00:49 +0530

    Added edit expense component's basic layout

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const EditExpensePage = () => (
+    <div>
+        edit expense page
+    </div>
+)
+
+export default EditExpensePage;
