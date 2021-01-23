33d97838324ff0f0c9e7e8d0f70f9ae90b5f59f0
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-01-24 04:43:25 +0530

    Segmented the redux store into actions, reducers, services and store named folders

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
+import {addExpense, editExpense, removeExpense} from './actions/expenses';
+import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from './actions/filters'
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+})
+
+const expenseOne = store.dispatch(addExpense({ description: 'water bill', amount: 100, createdAt: 11200}));
+const expenseTwo = store.dispatch(addExpense({ description: 'gas bill', amount: 500, createdAt: 2000}));
+
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
