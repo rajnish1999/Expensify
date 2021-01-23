4e0a90762907a26ed4cd8b324f2984b28562de70
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-01-24 03:00:02 +0530

    Added dashboard component's basic layout

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is Dashboard
+    </div>
+);
+
+export default ExpenseDashboardPage;
