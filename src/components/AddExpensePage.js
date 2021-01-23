b9661d84b3be11f2bde54dcbc2233fb6bb401c67
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-01-24 03:00:33 +0530

    Added add expense component's basic layout

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const AddExpensePage = () => (
+    <div>
+        add expense page
+    </div>
+)
+
+export default AddExpensePage;
