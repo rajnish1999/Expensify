96719b1e4a787d26977bb66ae832742c54283c00
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-08 18:26:19 +0530

    Get expenses from firebase DB on start

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -38,3 +38,30 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 })
+
+export const setExpenses = (expenses) => {
+    
+    return ({
+        type: 'SET_EXPENSES',
+        expenses
+    })
+}
+
+export const startSetExpenses = () => {
+    console.log("here");
+    return (dispatch) => {
+       return database.ref('expenses').once('value').then((snapshot) => {
+           const expenses = [];
+
+           snapshot.forEach((childSnapshot) => {
+               expenses.push({
+                   id: childSnapshot.key,
+                   ...childSnapshot.val()
+               })
+           })
+
+           dispatch(setExpenses(expenses));
+       })
+    }
+}
+
--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore';
-import {addExpense, editExpense, removeExpense} from './actions/expenses';
-import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from './actions/filters'
+import {startSetExpenses} from './actions/expenses';
+import {setTextFilter} from './actions/filters'
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -18,4 +18,10 @@ const jsx = (
     </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+})
+
+
